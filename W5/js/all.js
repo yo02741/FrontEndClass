afc3dc0727bb8202b0f47b88433197600498de76
YoChen <a0922335447@gmail.com>
2020-08-07 17:15:11 +0800

    W5 final

--- W5/js/all.js
+++ W5/js/all.js
@@ -1,5 +1,37 @@
+var FirstChecked = false;
 $(document).ready(function(){
     $('.button-toggle').click(function(){
         $('.menu').toggleClass('active')
     })
+
+    $('#ElectronicInvoice').click(function(e){
+        e.preventDefault();
+        if(FirstChecked == false){
+            $(this).toggleClass('active')
+            FirstChecked = true
+        }else{
+            if($(this).hasClass('active')){
+                return
+            }else{
+                $(this).toggleClass('active')
+                $('#MailInvoice').toggleClass('active')
+            }
+        }
+    })
+
+    $('#MailInvoice').click(function(e){
+        e.preventDefault();
+        if(FirstChecked == false){
+            $(this).toggleClass('active')
+            FirstChecked = true
+        }else{
+            if($(this).hasClass('active')){
+                return
+            }else{
+                $(this).toggleClass('active')
+                $('#ElectronicInvoice').toggleClass('active')
+            }
+        }
+    })
+
 })
